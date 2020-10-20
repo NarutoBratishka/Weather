221e27ae228d612dac6c22ecda0c3c4b6ff4193e
NarutoBratishka <katorabian@gmail.com>
2020-10-20 16:10:18 +0700

    add functionality: URL, JSON

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,16 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import org.json.JSONObject;
 
 public class Controller {
 
@@ -46,7 +50,45 @@ public class Controller {
     @FXML
     void initialize() {
         getData.setOnAction(event -> {
-            System.out.println("Всё работает!");
+            String getUserCity = city.getText().trim();
+
+            if (!getUserCity.equals("")) {
+                String output = getUrlContent("http://api.openweathermap.org/data/2.5/weather?q=" + getUserCity + "&appid=82b797b6ebc625032318e16f1b42c016&units=metric");
+                if (!output.isEmpty()) {
+                    image.setFitHeight(image.getFitHeight()/2);
+                    image.setFitWidth(image.getFitWidth()/2);
+                    image.setX(image.getX() + image.getFitWidth());
+                    image.setY(image.getY() + image.getFitHeight());
+
+                    JSONObject obj = new JSONObject(output);
+                    temp_info.setText("Температура: " + obj.getJSONObject("main").getDouble("temp"));
+                    temp_feels.setText("Ощущается: " + obj.getJSONObject("main").getDouble("feels_like"));
+                    temp_max.setText("Макс.: " + obj.getJSONObject("main").getDouble("temp_max"));
+                    temp_min.setText("Мин.: " + obj.getJSONObject("main").getDouble("temp_min"));
+                    pressure.setText("Давление: " + obj.getJSONObject("main").getDouble("pressure"));
+                }
+            }
+//            image.setVisible(false);
         });
+    }
+
+    private static String getUrlContent(String urlAdress) {
+        StringBuffer content = new StringBuffer();
+
+        try {
+            URL url = new URL(urlAdress);
+            URLConnection urlCon = url.openConnection();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Город не найден");
+        }
+        return content.toString();
     }
 }
