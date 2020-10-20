07373af1d33a9c1e60da33875041f8bcdf71289f
NarutoBratishka <katorabian@gmail.com>
2020-10-20 12:44:42 +0700

    added skeleton of controller changed .gitignore

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,59 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Button getData;
+
+    @FXML
+    private Text temp_info;
+
+    @FXML
+    private Text temp_feels;
+
+    @FXML
+    private Text temp_max;
+
+    @FXML
+    private Text temp_min;
+
+    @FXML
+    private Text pressure;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    void initialize() {
+        assert logo != null : "fx:id=\"logo\" was not injected: check your FXML file 'sample.fxml'.";
+        assert city != null : "fx:id=\"city\" was not injected: check your FXML file 'sample.fxml'.";
+        assert getData != null : "fx:id=\"getData\" was not injected: check your FXML file 'sample.fxml'.";
+        assert temp_info != null : "fx:id=\"temp_info\" was not injected: check your FXML file 'sample.fxml'.";
+        assert temp_feels != null : "fx:id=\"temp_feels\" was not injected: check your FXML file 'sample.fxml'.";
+        assert temp_max != null : "fx:id=\"temp_max\" was not injected: check your FXML file 'sample.fxml'.";
+        assert temp_min != null : "fx:id=\"temp_min\" was not injected: check your FXML file 'sample.fxml'.";
+        assert pressure != null : "fx:id=\"pressure\" was not injected: check your FXML file 'sample.fxml'.";
+        assert image != null : "fx:id=\"image\" was not injected: check your FXML file 'sample.fxml'.";
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Погодник");
-        primaryStage.setScene(new Scene(root, 380, 615));
+        primaryStage.setScene(new Scene(root, 368, 587));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
