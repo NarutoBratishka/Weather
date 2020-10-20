6c02893f90df30258ff03e71cebd251c5300cefd
NarutoBratishka <katorabian@gmail.com>
2020-10-20 12:50:16 +0700

    add console out for testing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -45,15 +45,8 @@ public class Controller {
 
     @FXML
     void initialize() {
-        assert logo != null : "fx:id=\"logo\" was not injected: check your FXML file 'sample.fxml'.";
-        assert city != null : "fx:id=\"city\" was not injected: check your FXML file 'sample.fxml'.";
-        assert getData != null : "fx:id=\"getData\" was not injected: check your FXML file 'sample.fxml'.";
-        assert temp_info != null : "fx:id=\"temp_info\" was not injected: check your FXML file 'sample.fxml'.";
-        assert temp_feels != null : "fx:id=\"temp_feels\" was not injected: check your FXML file 'sample.fxml'.";
-        assert temp_max != null : "fx:id=\"temp_max\" was not injected: check your FXML file 'sample.fxml'.";
-        assert temp_min != null : "fx:id=\"temp_min\" was not injected: check your FXML file 'sample.fxml'.";
-        assert pressure != null : "fx:id=\"pressure\" was not injected: check your FXML file 'sample.fxml'.";
-        assert image != null : "fx:id=\"image\" was not injected: check your FXML file 'sample.fxml'.";
-
+        getData.setOnAction(event -> {
+            System.out.println("Всё работает!");
+        });
     }
 }
